b84a624f2eeba92232c080298d717c72d5f14f83
uwababyeyi <nuwababyeyi@gmail.com>
2023-08-10 09:56:37 -0700

    correction of five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-
 #include<stdio.h>
 /**
 * main -enrty point
-*return: Always 0 (when the program is successful)
+*Return: 0 (success)
 */
- int main(void)
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
