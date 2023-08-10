c28a702071e7671648e6472e3e11e66f825afbc6
uwababyeyi <nuwababyeyi@gmail.com>
2023-08-10 05:01:04 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+* main -enrty point
+*return: Always 0 (when the program is successful)
+*/
+main (void){
+puts(\n "Programming is like building a multilingual puzzle");
+return (0);}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+* main -enrty point
+*return: Always 0 (when the program is successful)
+*/
+main (void){
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);}
