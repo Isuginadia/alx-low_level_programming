7a15492e6f9d1658fd74f2761d2aab94fa33e281
uwababyeyi <nuwababyeyi@gmail.com>
2023-08-11 04:25:12 -0700

    correction of 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 
 #include<stdio.h>
 /**
-* main -enrty point
-*return: Always 0 (when the program is successful)
+* main - Enrty point
+* Return: Always 0 (success)
 */
 int main(void)
 {
-printf("Size of a char: %d byte(s)1 \n",sizeof(char));
-printf("Size of an int: %d byte(s)1 \n",sizeof(int));
-printf("Size of a long int: %d byte(s)1 \n",sizeof(long int));
-printf("Size of a long long int: %d byte(s)1 \n",sizeof(long long int));
-printf("Size of a float:%d byte(s)1 \n",sizeof(float));
+printf("Size of a char: %lu\r\n", sizeof(char));
+printf("Size of an int: %lu\r\n", sizeof(int));
+printf("Size of a long int: %lu\r\n", sizeof(long int));
+printf("Size of a long long int: %lu\r\n", sizeof(long long int));
+printf("Size of a float: %lu\r\n", sizeof(float));
 return (0);
 }
